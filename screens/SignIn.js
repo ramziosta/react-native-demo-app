e74512268533e0b2494cb045ac0a3fe34f0db4c2
Ramzi Osta <76601606+ramziosta@users.noreply.github.com>
2022-12-07 13:40:37 -0500

    firebase user registration and login works

--- screens/SignIn.js
+++ screens/SignIn.js
@@ -1,17 +1,68 @@
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
-import { StyleSheet, View, Text, Button } from "react-native";
+import { StyleSheet, View, Text, TextInput, Button } from "react-native";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const SignInScreen = (props) => {
+  const auth = getAuth();
   const nav = useNavigation();
+
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [user, setUser] = React.useState("");
+  
+  const handleLogin = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        setUser(user);
+        nav.navigate("Home");
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  };
 
   return (
     <View style={styles.layout}>
       <Text style={styles.title}>Sign In Screen</Text>
-      <Button
-        title="Sign Up"
-        onPress={() => props.navigation.navigate("SignUp")}
-      />
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder="email"
+          keyboardType="email-address"
+          value={email}
+          onChangeText={(email) => {
+            setEmail(email);
+          }}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="password"
+          keyboardType="password-address"
+          value={password}
+          onChangeText={(password) => {
+            setPassword(password);
+          }}
+          secureTextEntry={true}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <Button title="Login" onPress={handleLogin} />
+      </View>
+      <View>
+        <Text style={styles.text}>Dont have an account? </Text>
+        <Button
+          title="Register"
+          onPress={() => props.navigation.navigate("SignUp")}
+        />
+      </View>
     </View>
   );
 };
@@ -29,5 +80,18 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: "bold",
     textAlign: "center",
+  },
+  text: {
+    margin: 24,
+    fontSize: 14,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  input: {
+    borderColor: "gray",
+    width: "100%",
+    borderBottomWidth: 1,
+    borderRadius: 10,
+    padding: 10,
   },
 });
